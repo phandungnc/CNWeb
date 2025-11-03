00878e32dfb2b10adf5d8bde0829dc1fcaddee70
phandungnc <phanducdung2004@gmail.com>
2025-11-03 18:51:01 +0700

    dk, dn

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/login" element={<LoginPage />} />
             </Routes>
         </Router>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Import toàn bộ CSS gốc
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/file-upload.css";
+import "./assets/css/plyr.css";
+import "./assets/css/full-calendar.css";
+import "./assets/css/jquery-ui.css";
+import "./assets/css/editor-quill.css";
+import "./assets/css/jquery-jvectormap-2.0.5.css";
+import "./assets/css/main.css";
+
+// Tự load JS tĩnh
+function loadScripts() {
+  const scripts = [
+    "/src/assets/js/jquery-3.7.1.min.js",
+    "/src/assets/js/bootstrap.bundle.min.js",
+    "/src/assets/js/phosphor-icon.js",
+    "/src/assets/js/main.js",
+  ];
+
+  scripts.forEach((src) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.async = false;
+    document.body.appendChild(script);
+  });
+}
+loadScripts();
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
