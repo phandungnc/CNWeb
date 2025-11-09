ce5a8b64f5d9d8f89eb2edcd4d58d21fe7c24b2e
phandungnc <phanducdung2004@gmail.com>
2025-11-09 22:01:19 +0700

    trang home sau dang nhap

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/home" element={<HomePage />} />
             </Routes>
         </Router>
     );
