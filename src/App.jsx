ff6d800959d504c2da61a9500f2f6d5a52997ceb
phandungnc <phanducdung2004@gmail.com>
2025-11-16 23:29:25 +0700

    admin, course, assg

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import AdminPage from "./pages/AdminPage";
+import CoursePage from "./pages/CoursePage";
+import CreateCoursePage from "./pages/CreateCoursePage";
+import CreateQuizPage from "./pages/CreateQuizPage";
+import AssignmentPage from "./pages/AssignmentPage";
 
 function App() {
     return (
@@ -12,6 +17,11 @@ function App() {
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/home" element={<HomePage />} />
+                <Route path="/admin" element={<AdminPage />} />
+                <Route path="/course/:id" element={<CoursePage />} />
+                <Route path="/create-course" element={<CreateCoursePage />} />
+                <Route path="/create-quiz" element={<CreateQuizPage />} />
+                <Route path="/assignment/:id" element={<AssignmentPage />} />
             </Routes>
         </Router>
     );
